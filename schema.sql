89d800df3800fe19f1d796ce7e60394f25e9dc34
Florian Polster <florian.polster@staffbase.com>
2020-06-15 18:48:11 +0200

    Finish schema revision draft

--- schema.sql
+++ schema.sql
@@ -1,23 +1,47 @@
-CREATE TABLE proxies ()
-CREATE TABLE providers ()
-CREATE TABLE test_urls ()
-CREATE TABLE checks ()
-CREATE VIEW proxy_details (
-    -- IP address
-    -- port number
-    -- date of the last successful basic functionality test
-    -- date when the address was last found in any proxy list
-    -- date when the address was first found in any proxy list
+CREATE TABLE fetch_runs (
+    provider_url TEXT,
+    proxy TEXT,
+    ts TIMESTAMP
 )
-CREATE VIEW provider_details (
-    -- base address (URL) of the proxy list
-    -- details on extracting the HTTP(S) proxy addresses from the list
-    -- date of the last successful update of the proxy list
-    -- date of the last attempt to update with the number of records found
-    -- indication of an error that occurred during the update latest attempt
+CREATE TABLE checks (
+    proxy TEXT,
+    test_url TEXT,
+    ts TIMESTAMP NOT NULL,
+    success BOOLEAN NOT NULL,
+    status_code NUMBER,
+    error_msg TEXT
 )
-CREATE VIEW test_url_details (
-    -- test URL
-    -- details of functionality test validation
-    -- the date of the last successful functionality test
+CREATE VIEW proxy_details AS (
+    WITH check_stats AS (
+        SELECT
+            proxy,
+            MAX(ts) AS last_success
+        FROM checks
+        WHERE success = true
+        GROUP BY proxy
+    ),
+    fetch_stats AS (
+        SELECT
+            proxy,
+            MAX(ts) AS last_seen,
+            MIN(ts) AS first_seen
+        FROM fetch_runs
+        GROUP BY proxy
+    )
+    SELECT
+        c.proxy AS proxy, -- IP address, port number
+        c.last_success AS last_success, -- date of the last successful basic functionality test
+        f.last_seen AS last_seen, -- date when the address was last found in any proxy list
+        f.first_seen AS first_seen,-- date when the address was first found in any proxy list
+    FROM check_stats AS c
+    JOIN fetch_stats AS f
+    ON c.proxy = f.proxy
+)
+CREATE VIEW provider_details AS (
+    SELECT
+        provider_url, -- base address (URL) of the proxy list
+        MAX(ts) AS last_update, -- date of the last successful update of the proxy list
+        COUNT(proxy) AS last_count -- date of the last attempt to update with the number of records found
+    FROM fetch_runs
+    GROUP BY provider_url
 )
