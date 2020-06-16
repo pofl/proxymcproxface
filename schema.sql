8f376964fa9e900629784199d7cc2b382958f46d
Florian Polster <florian.polster@staffbase.com>
2020-06-16 10:47:36 +0200

    Fix schema draft after actually trying to run it on DB

--- schema.sql
+++ schema.sql
@@ -2,15 +2,15 @@ CREATE TABLE fetch_runs (
     provider_url TEXT,
     proxy TEXT,
     ts TIMESTAMP
-)
+);
 CREATE TABLE checks (
     proxy TEXT,
     test_url TEXT,
     ts TIMESTAMP NOT NULL,
     success BOOLEAN NOT NULL,
-    status_code NUMBER,
+    status_code INTEGER,
     error_msg TEXT
-)
+);
 CREATE VIEW proxy_details AS (
     WITH check_stats AS (
         SELECT
@@ -32,11 +32,11 @@ CREATE VIEW proxy_details AS (
         c.proxy AS proxy, -- IP address, port number
         c.last_success AS last_success, -- date of the last successful basic functionality test
         f.last_seen AS last_seen, -- date when the address was last found in any proxy list
-        f.first_seen AS first_seen,-- date when the address was first found in any proxy list
+        f.first_seen AS first_seen -- date when the address was first found in any proxy list
     FROM check_stats AS c
     JOIN fetch_stats AS f
     ON c.proxy = f.proxy
-)
+);
 CREATE VIEW provider_details AS (
     SELECT
         provider_url, -- base address (URL) of the proxy list
@@ -44,4 +44,4 @@ CREATE VIEW provider_details AS (
         COUNT(proxy) AS last_count -- date of the last attempt to update with the number of records found
     FROM fetch_runs
     GROUP BY provider_url
-)
+);
