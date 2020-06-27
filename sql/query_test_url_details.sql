1621ae0ddbf7f61dfdc99c6279b9962392f9526d
Florian Polster <florian.polster@staffbase.com>
2020-06-27 16:44:35 +0200

    Add testurls details to frontend

--- sql/query_test_url_details.sql
+++ sql/query_test_url_details.sql
@@ -1,11 +1,21 @@
 -- get the any of the most recently working proxies and indicate whether they worked during the last run
-SELECT
+with pre as (
+    select
+        test_url,
+        proxy,
+        success,
+        this_proxy_check_start,
+	    check_start,
+        max(check_start) over (partition by test_url) as most_recent_run
+    from checks
+)
+select
     test_url,
     proxy,
-    this_proxy_check_start,
-    check_start = (SELECT MAX(check_start) FROM checks) AS is_most_recent_run
-FROM checks
-WHERE test_url = $1
-    AND success IS TRUE
-ORDER BY this_proxy_check_start DESC
-LIMIT 1
+    this_proxy_check_start as last_success,
+    check_start = most_recent_run AS is_most_recent_run
+from pre
+where success is true
+    and test_url = $1
+order by this_proxy_check_start desc
+limit 1
